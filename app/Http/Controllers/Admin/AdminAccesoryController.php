4ca2375dc83065c760b96147fe04f9f05dd8c40c
ntvelezd <53051040+ntvelezd@users.noreply.github.com>
2021-09-25 19:40:52 -0500

    CRUD Accesory3

--- app/Http/Controllers/Admin/AdminAccesoryController.php
+++ app/Http/Controllers/Admin/AdminAccesoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Accesory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
@@ -23,6 +24,46 @@ class AdminAccesoryController extends Controller
 
     public function catalogue()
     {
-        return view('admin.accesory.catalogue');
+        $accesory = Accesory::all();
+        $data["title"] = "Accesory";
+        $data["Accesory"] = $accesory;
+        return view('admin.accesory.catalogue')->with("data", $data);
+    }
+    public function createAccesory()
+    {
+        return view('admin.accesory.create');
+    }
+
+    public function saveAccesory(Request $request)
+    {
+        Accesory::validate(($request));
+        Accesory::create($request->only(["name","price","image"]));
+        $message = 'Accesorio creado satisfactoriamente';
+        return view('admin.accesory.save')->with("message", $message);
+    }
+
+    public function editAccesory($id)
+    {
+        $accesory = Accesory::findOrFail($id);
+        $data["title"] = $accesory->getName();
+        $data["Accesory"] = $accesory;
+        return view('admin.accesory.edit')->with("data", $data);
+    }
+
+    public function saveEditAccesory(Request $request)
+    {
+        Accesory::validate(($request));
+        $accesory = Accesory::findOrFail($request['id']);
+        $accesory->fill($request->only(["name","price","items"]));
+        $accesory->save();
+        $message = 'Accesorio editado satisfactoriamente';
+        return view('admin.accesory.saveEditAccesory')->with("message", $message);
+    }
+
+    public function deleteAccesory(Request $request)
+    {
+        Accesory::destroy($request->only(["id"]));
+        $message = 'Accesorio borrado satisfactoriamente';
+        return view('admin.accesory.delete')->with("message", $message);
     }
 }
